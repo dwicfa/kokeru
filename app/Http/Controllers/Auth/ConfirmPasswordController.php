007611ed2bd61371092ee194e94e0730dfc56197
AdamSukma <adamsukma10@gmail.com>
2020-12-02 17:31:47 +0700

    delete controller template

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -26,7 +26,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = 'managers/dashboard';
 
     /**
      * Create a new controller instance.
